f57e73b8a3a0de1bae692dd70c8b49038d90af03
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-09-19 22:39:09 -0700

    resource stuff

---
+++ candyshop/Script/itemResources.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name ItemResource
+
+@export var Name : String
+@export var Amount : int
---
+++ candyshop/Script/shelfResource.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name ShelfResource
+
+@export var item_array : Array[ItemResource]
