91e901e5c8cd17b845d906ec26487aa7bf72024c
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-09-19 23:44:27 -0700

    resourse and buttons

---
+++ candyshop/Scenes/shelf_buttons.gd
@@ -0,0 +1,13 @@
+extends GridContainer
+
+var holdingCorn : bool
+@export var shelfResource : ShelfResource
+
+var button_preload = preload('res://Scenes/shelf_button.tscn')
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in 2:
+		var newbutton = button_preload.instantiate()
+		newbutton.init(shelfResource.item_array[i].Name)
+		add_child(newbutton)
