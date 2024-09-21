1ccbceb805518a13ac3201a489a001a87e70c6e0
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-09-21 14:42:55 -0700

    working on dialogue manager
    made all the resources for now

---
+++ candyshop/Script/Dialogue Machine/dialogue_manager.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	orderStart()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func orderStart():
+	#how many npcs we have in Dialogue Array
+	var npc = randi_range(0,0)
+	print(npc)
+	pass
