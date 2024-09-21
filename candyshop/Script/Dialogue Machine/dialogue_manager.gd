36a2a84138b33b1b6bc45e5de14de18d95096473
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-09-21 15:04:21 -0700

    Dialogue Manager somewhat done

--- candyshop/Script/Dialogue Machine/dialogue_manager.gd
+++ candyshop/Script/Dialogue Machine/dialogue_manager.gd
@@ -1,5 +1,7 @@
 extends Control
-
+@export var dialogueArray : DialogueArray
+@export var textLabel : RichTextLabel
+@export var timer : Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,10 +12,31 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+var npc
 func orderStart():
+	print('orderStart')
 	#how many npcs we have in Dialogue Array
-	var npc = randi_range(0,0)
+	npc = randi_range(0,0)
 	print(npc)
+	var name = dialogueArray.npc_array[npc].Name
+	var arrayamount = dialogueArray.npc_array[npc].text.size()
+	playText()
 	
+var i = 0
+func playText():
+	print('playText')
+	var arrayamount = dialogueArray.npc_array[npc].text.size()
+	var text
 	
+	print(i)
+	if arrayamount > i:
+		text = dialogueArray.npc_array[npc].text[i]
+		textLabel.text = text
+		timer.start()
+		print
+		i += 1
+
+
+func _on_dialogue_timer_timeout() -> void:
+	print('timeout')
+	playText()
