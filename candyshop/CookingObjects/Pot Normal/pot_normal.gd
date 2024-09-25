dec5df02696419daa62b32c7dd52d0fd48401aeb
Timmytheghosty <157427377+Timmytheghosty@users.noreply.github.com>
2024-09-24 17:32:43 -0700

    Pot system done
    The timer works collecting works all thats needed is a progress bar for the timer and to connect to luna's inv

--- candyshop/CookingObjects/Pot Normal/pot_normal.gd
+++ candyshop/CookingObjects/Pot Normal/pot_normal.gd
@@ -14,38 +14,72 @@ var collected = false
 func addToStorage(nameOf):
 	GameManager.send_Amount(nameOf)
 
+
+func resetTimer():
+	rottingTimer.stop()
+	rottingTimer.wait_time = 5 #set this to what is on rotting timer node
+	# resets the timer when it's collected
+
 func switchSprite():
+	plant.visible = false
+	sprout.visible = false
+	
+	
 	if (growing == true):
-		sprout.visible = true
 		growingTimer.start()
+		sprout.visible = true
+		print("growing")
 	elif(rotting == true):
-		plant.visible = true
 		rottingTimer.start()
+		plant.visible = true
+		print('rotting')
 		if (collected == true):
-			print("cool")
-			collected == false
+			print("collected")
+			collected = false
 			
 			addToStorage(Chocolate)
+			print(Chocolate)
+	
+
+
+	
+func _on_rotting_timer_timeout():
+	print("rotting timer")
+	rotting = false
+	growing = true
+	
+	switchSprite()
 	
 
 
 func _on_growing_timer_timeout():
+	print('growing timer')
 	growing = false
 	rotting = true
-	sprout.visible = false
-	switchSprite()
 	
-func _on_rotting_timer_timeout():
-	rotting =false
-	growing = true
-	plant.visible = false
 	switchSprite()
-	
+
 
 func _ready():
 	switchSprite()
 
 
 func _on_collect_button_button_down():
-	print("worked")
-	collected = true
+	if (rotting == true):
+		print("can collect")
+		collected = true
+		rotting = false
+		growing = true
+		resetTimer()
+		switchSprite()
+		
+	else:
+		print("can't collect yet")
+		
+	
+
+
+
+func _on_collect_button_button_up() -> void:
+	print("done")
+	collected = false
