ad7d36be0f01389bd708c96de692173c5bf9e3a1
Timmytheghosty <157427377+Timmytheghosty@users.noreply.github.com>
2024-09-24 22:57:57 -0700

    day system
    luna told me to push it in scary voice

--- candyshop/Scenes/Playscreen/play_screen.gd
+++ candyshop/Scenes/Playscreen/play_screen.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 @export var camera: Camera2D  
 var target_position: Vector2
+var customers = 5
+var sell = 0
+var miss  = 0
+
+
 
 func _ready():
 	target_position = camera.position  
@@ -14,3 +19,13 @@ func back_pressed():
 
 func _process(delta):
 	camera.position = camera.position.lerp(target_position, 0.1)
+	if (sell+miss == customers):
+		if (sell >= 3):
+			pass
+
+
+
+
+func _on_sell_button_down() -> void:
+	pass
+	
