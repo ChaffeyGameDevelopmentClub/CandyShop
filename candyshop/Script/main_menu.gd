c686bdf7c62469faea8834e06d981998eea6b3f6
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-09-25 00:01:12 -0700

    connected main menu
    able to release a demo

--- candyshop/Script/main_menu.gd
+++ candyshop/Script/main_menu.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/shop_screen.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Playscreen/play_screen.tscn")
 
 
 func _on_options_button_pressed() -> void:
