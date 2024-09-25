f5ceaa8b7f6733328b7b770137a7f525eef8f7b7
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-09-24 19:06:11 -0700

    idk rn

--- candyshop/Script/main_menu.gd
+++ candyshop/Script/main_menu.gd
@@ -18,8 +18,8 @@ func _on_play_button_pressed() -> void:
 
 
 func _on_options_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
+	pass # Replace with function body.
 
 
 func _on_quit_button_pressed() -> void:
-	get_tree().quit()
+	pass # Replace with function body.
