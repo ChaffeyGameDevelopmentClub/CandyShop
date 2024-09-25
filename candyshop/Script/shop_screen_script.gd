8e811d64c44b579570d70f1be9f1afcd8344e274
QTheGuy000 <qlebovic6505@panther.chaffey.edu>
2024-09-24 21:11:12 -0700

    Customer randomly orders item
    Description

--- candyshop/Script/shop_screen_script.gd
+++ candyshop/Script/shop_screen_script.gd
@@ -1,12 +1,14 @@
 extends Node2D
 var itemOrdered
-var itemMade
-var itemAdded
+var rng = RandomNumberGenerator.new()
+var items1 = ["Chocolate Popcorn", "Chocolate Bears", "Bear Pop", "Gummy-Pops", "Gummy Sharks", "Chocolate Forest", "Gingerbread House", "Gummy Army", "Strawberry Fields", "Chocolate Latte", "Gummy Candy Corn", "Swedish Fish", "Caramel Apples", "Ginger Bread Man", "last"]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass#GameManager.connect('sendAmount',itemAdded)
+	_makeOrder()
 
-
+func _makeOrder():
+	itemOrdered = items1[rng.randf_range(0,14)]
+	print(itemOrdered)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
