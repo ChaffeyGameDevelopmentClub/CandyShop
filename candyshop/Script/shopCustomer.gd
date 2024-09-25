0631fbe71b29c39cc6d2546d41cd367e6cb34ace
QTheGuy000 <qlebovic6505@panther.chaffey.edu>
2024-09-24 21:16:25 -0700

    Customer triggers order
    Updating this for reasons

--- candyshop/Script/shopCustomer.gd
+++ candyshop/Script/shopCustomer.gd
@@ -2,11 +2,13 @@ extends Sprite2D
 var customerMovementState
 var order
 var dialogueManager
+var shopScreen
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	customerMovementState = 1
 	order = get_tree().current_scene.get_node("OrderRect")
 	dialogueManager = get_tree().current_scene.get_node("DialogueManager")
+	shopScreen = get_tree().current_scene#.get_node("ShopScreen")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,7 +17,7 @@ func _process(delta):
 		position += Vector2(10, 0)
 	if(position.x>1499 and customerMovementState == 1):
 		customerMovementState = 2
-		print("ShopCustomerScript")
+		shopScreen._makeOrder()
 		dialogueManager.orderStart()
 	
 func dialogueEnd():
--- candyshop/Script/shop_screen_script.gd
+++ candyshop/Script/shop_screen_script.gd
@@ -4,11 +4,11 @@ var rng = RandomNumberGenerator.new()
 var items1 = ["Chocolate Popcorn", "Chocolate Bears", "Bear Pop", "Gummy-Pops", "Gummy Sharks", "Chocolate Forest", "Gingerbread House", "Gummy Army", "Strawberry Fields", "Chocolate Latte", "Gummy Candy Corn", "Swedish Fish", "Caramel Apples", "Ginger Bread Man", "last"]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_makeOrder()
+	pass
 
 func _makeOrder():
 	itemOrdered = items1[rng.randf_range(0,14)]
-	print(itemOrdered)
+	print("ordered: " + itemOrdered)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
