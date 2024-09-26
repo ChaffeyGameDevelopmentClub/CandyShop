79957f4e4b34395f329ad1685336b7e27ada3952
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-09-25 18:25:14 -0700

    fixed both loop and added name

--- candyshop/Script/Dialogue Machine/dialogue_manager.gd
+++ candyshop/Script/Dialogue Machine/dialogue_manager.gd
@@ -3,6 +3,7 @@ extends Control
 @export var textLabel : RichTextLabel
 @export var timer : Timer
 @export var Customer : Sprite2D
+@export var nameText : RichTextLabel
 
 var itemName
 # Called when the node enters the scene tree for the first time.
@@ -15,41 +16,50 @@ func _process(delta: float) -> void:
 	pass
 
 var npc
+var npcName
 func orderStart():
 	#print('orderStart')
 	#how many npcs we have in Dialogue Array
 	npc = randi_range(0,1)
-	#print(npc)
+	
 	var name = dialogueArray.npc_array[npc].Name
 	var arrayamount = dialogueArray.npc_array[npc].text.size()
-	var npcName = dialogueArray.npc_array[npc].Name
+	npcName = dialogueArray.npc_array[npc].Name
+	
 	playText()
+	print_debug(npcName)
+	nameText.text = npcName
 	#print_debug("orderStart Done")
 	return npcName
 	
 var i = 0
 var arrayamount
 func playText():
+	
 	#print_debug('playText')
 	arrayamount = dialogueArray.npc_array[npc].text.size()
 	#print_debug(arrayamount)
-	print_debug(i)
+	#print_debug(i)
 	var text
 	
 	#print(i)
 	if arrayamount > i and !arrayamount-1 == i:
 		text = dialogueArray.npc_array[npc].text[i]
-		#print_debug(text)
-		textLabel.text = text
+		#print_debug('First')
+		textLabel.text ='[center]' + text + '[/center]'
 		timer.start()
 		#print
 		i += 1
 	elif arrayamount-1 == i:
 		text = dialogueArray.npc_array[npc].text[i]
-		textLabel.text = text + itemName
+		textLabel.text = '[center]' + text + itemName + '[/center]'
 		Customer.dialogueEnd()
+		timer.stop()
 		#print(str(i) + " arrayamount")
+		#print_debug('Second')
 		i = 0
+		
+		
 func sendToDialogue(itemName1):
 	itemName = itemName1
 
@@ -60,7 +70,7 @@ func failedOrder():
 	textLabel.text = "you failed me"
 
 func _on_dialogue_timer_timeout() -> void:
-	print_debug('timeout')
+	#print_debug('timeout')
 	playText()
 
 
